ff0c24d96d02b8fd63842997c056a5716242fbfd
Subhajitsdev <120780641+Subhajitsdev@users.noreply.github.com>
2023-06-07 00:07:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ quoteBtn = document.querySelector("button"),
 authorName = document.querySelector(".name"),
 speechBtn = document.querySelector(".speech"),
 copyBtn = document.querySelector(".copy"),
-
+twitterBtn = document.querySelector(".twitter"),
 synth = speechSynthesis;
 
 function randomQuote(){
@@ -31,5 +31,9 @@ copyBtn.addEventListener("click", ()=>{
     navigator.clipboard.writeText(quoteText.innerText);
 });
 
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
 
 quoteBtn.addEventListener("click", randomQuote);
